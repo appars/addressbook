0eef9ebf15c777fab64e130db7b88f6ed301cd0c
Apparsamy Perumal <apparsamyperumal@Apparsamys-MacBook-Air.local>
2018-02-20 04:42:30 +0530

    Version Change 4.7

--- src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
@@ -71,7 +71,7 @@ public class AddressbookUI extends UI {
          */
         newContact.addClickListener(e -> contactForm.edit(new Contact()));
 
-        filter.setInputPrompt("Addressbook Release 4.6");
+        filter.setInputPrompt("Addressbook Release 4.7");
         filter.addTextChangeListener(e -> refreshContacts(e.getText()));
 
         contactList
