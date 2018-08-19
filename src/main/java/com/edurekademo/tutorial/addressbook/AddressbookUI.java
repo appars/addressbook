e05dad17d6a8a297056363982beaf2ab69aea473
Appar <32193913+appars@users.noreply.github.com>
2018-08-19 12:02:54 +0530

    Update AddressbookUI.java

--- src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
@@ -71,7 +71,7 @@ public class AddressbookUI extends UI {
          */
         newContact.addClickListener(e -> contactForm.edit(new Contact()));
 
-        filter.setInputPrompt("Addressbook Release V6.0 on 5th April 2018 at 12PM");
+        filter.setInputPrompt("Addressbook Release V6.1");
         filter.addTextChangeListener(e -> refreshContacts(e.getText()));
 
         contactList
