ee1adde490d9e348af4de99eb6f15e9c4f66061b
Appar <32193913+appars@users.noreply.github.com>
2019-01-19 11:43:39 +0530

    Update AddressbookUI.java

--- src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
@@ -71,7 +71,7 @@ public class AddressbookUI extends UI {
          */
         newContact.addClickListener(e -> contactForm.edit(new Contact()));
 
-        filter.setInputPrompt("Addressbook Release V10.5");
+        filter.setInputPrompt("Addressbook Release V11.0 after docker integration");
         filter.addTextChangeListener(e -> refreshContacts(e.getText()));
 
         contactList
